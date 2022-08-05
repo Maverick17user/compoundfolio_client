979bf5a8672f87e9e76b9181e8f5b7ae339d840a
Maverick17user <maksim.epifanov.2000@mail.ru>
2022-08-05 20:13:29 +0300

    Add back-end todo-tasks for an upcoming week

--- pages/api/auth/signUp.ts
+++ pages/api/auth/signUp.ts
@@ -4,11 +4,24 @@ type ResponseData = {
   message: string;
 };
 
+// TODO: 1
+// Choose & Init DB
+// Crate DB User schema
+// Crate GQL User schema
+// [ Front ] Crate GQL User resolver (with JWT, to sign-in the user after req.)
+
 export default function signUp(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   if (req.method === "GET") {
+    // TODO: 2
+    // Validate the req.body data
+    // Return errors
+    // Bcrypt the password
+    // Add user to the DB
+    // Generate JWT
+    // Send 200 status with JWT to the client
     res.status(200).json({ message: "John Doe" });
   }
 }
